67cdee9145fb526a781a6759d72d771cf763d2d9
xxxx <2458348401@qq.com>
2026-05-11 16:02:07 +0800

    @ fix: set COOP header to allow Google OAuth popup @

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    },
+  },
 })
